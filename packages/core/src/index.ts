ec703cb3f2c82d01dfffc81dc5f44832389a74fa
avchugaev <achugaev93@gmail.com>
2022-09-07 20:51:04 +0300

    feat(core): :sparkles: add `Either` monad

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,7 @@
 export * from './lib/Func';
 export * from './lib/pipe';
 
+export * from './lib/monad/Either';
 export * from './lib/monad/IO';
 export * from './lib/monad/Optional';
 export * from './lib/monad/Validated';
