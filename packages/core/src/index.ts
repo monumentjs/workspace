5ef70c8686ffc448789e356ee7229c2f3d7e52c8
avchugaev <achugaev93@gmail.com>
2022-09-17 15:42:10 +0300

    refactor(core): :recycle: export `compose` function

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,7 @@
 export * from './lib/Falsy';
 export * from './lib/Func';
 export * from './lib/NonFalsy';
+export * from './lib/compose';
 export * from './lib/pipe';
 
 export * from './lib/monad/AIO';
